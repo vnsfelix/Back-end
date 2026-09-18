9133bcdd3fd60e5f290259f3a46da6b5fc359dc6
Felix <vfelix2022santos@gmail.com>
2026-09-18 11:10:10 -0300

    finalização

--- AULA02/conceito_tipos_dados.js
+++ AULA02/conceito_tipos_dados.js
@@ -9,3 +9,20 @@
 // Array (VETORES)
 // Null (NULO)
 // Undefined (NÃO DEFINIDO)
+
+// String (TEXTOS)
+let nome = "Jarvis"
+let mensagem = "Olá, mundo!"
+
+console.log(nome)
+
+let saudacao = "Bem-vindo, " + nome + "!"
+console.log(saudacao)
+
+// typeof
+// o typeof serve para verificar o tipo de dado de uma variável
+
+let nomeDois = "Felix"
+
+let soma = 10 + 5;
+console.log(soma)
---
+++ AULA02/ex01.js
@@ -0,0 +1,9 @@
+// Atividade Number
+// Crie uma variável chama anoAtual e outra chamada anoNascimento
+// calcule e exiba a idade no console
+
+let anoAtual = 2026
+let anoNascimento = 2009
+let idade = anoAtual - anoNascimento
+
+console.log(idade)
