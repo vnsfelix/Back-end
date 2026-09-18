9f925eb78b83b32ce7d5003282943f0b4323d648
Felix <vfelix2022santos@gmail.com>
2026-09-18 09:14:20 -0300

    backend

---
+++ AULA02/index.js
@@ -0,0 +1,21 @@
+// INICIO BACK END - JAVASCRIPT
+
+// VARIAVEIS
+
+// ANTIGO !!!!!!!!!, pode ser redeclarado e mudar o valor
+var idade = 25
+var idade = 30 // NO GERA ERRO
+
+// MAIS MODERNO - PODE MUDAR O VALOR, MAS NÃO PODE SER REDECLARADO
+let nome = "João"
+nome = "Maria"; //PERMITIDO
+let nome = "Pedro"; // ❌ ERRO
+
+
+// const é fixo, nao pode mudar o valor
+const PI = 3.14;
+// pi = 40; // ❌ ERRO
+
+
+
+
