3a34727a50f9a43d772fc4991db993af0ba6ed54
Felix <vfelix2022santos@gmail.com>
2026-09-18 09:38:17 -0300

    js add

---
+++ AULA02/conceito.js
@@ -0,0 +1,42 @@
+// INICIO BACK END - JAVASCRIPT
+
+// VARIAVEIS
+
+// ANTIGO !!!!!!!!!, pode ser redeclarado e mudar o valor
+var idade = 25
+var idade = 30 // NO GERA ERRO
+
+// MAIS MODERNO - PODE MUDAR O VALOR, MAS NÃO PODE SER REDECLARADO
+let nome = "João"
+nome = "Maria"; //PERMITIDO
+// let nome = "Pedro"; // ❌ ERRO
+
+
+// const é fixo, nao pode mudar o valor
+const PI = 3.14;
+// pi = 40; // ❌ ERRO
+
+
+// FORMAS DE ESCREVER UM CODIGO
+// ------------------------------------------
+
+// __________________________________________
+// CAMEL CASE ----- * A mais Famosa
+// - Primeira palavra minúscula
+// - Palavras Seguintees em maiúscula
+
+// let nomeCompleto; let idadeUsuario; functionCalcularIdade
+
+// __________________________________________
+// PASCALCASE
+// - Todas as palavras em maiúscula
+
+class UsuarioSistema {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+
+
--- AULA02/index.js
+++ AULA02/index.js
@@ -1,21 +1,11 @@
-// INICIO BACK END - JAVASCRIPT
+var cor = "azul";
+var cor = "vermelho";
+console.log(cor);
 
-// VARIAVEIS
+let fruta = "uva";
+fruta = "banana";
+console.log(fruta);
 
-// ANTIGO !!!!!!!!!, pode ser redeclarado e mudar o valor
-var idade = 25
-var idade = 30 // NO GERA ERRO
-
-// MAIS MODERNO - PODE MUDAR O VALOR, MAS NÃO PODE SER REDECLARADO
-let nome = "João"
-nome = "Maria"; //PERMITIDO
-let nome = "Pedro"; // ❌ ERRO
-
-
-// const é fixo, nao pode mudar o valor
 const PI = 3.14;
-// pi = 40; // ❌ ERRO
-
-
-
-
+// pi = 3.15; // ❌ ERRO
+console.log(PI);
